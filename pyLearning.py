5064b4fd3222c5bda5926c279e71fb1800b492a9
lenovo legion 5 pro <lenovolegion6pro@gmail.com>
2023-08-23 16:08:26 +0330

    Learning Python basic declaration rules

---
+++ pyLearning.py
@@ -0,0 +1,19 @@
+# Syntax highlighting: 
+
+#editor way of showing recongnized functions and showing them in custom colors.
+ 
+#Variables
+
+Variable_name = 'Value' # String, integer, float, Boolean
+
+# Variable rules to avoid name errors
+
+# Variable names can contain only letter, numbers and underscores (_).
+# a variable name cannot start with a number.
+# Spaces are not allowed in declaring Variable names.
+# Do not use Python preinstalled functions and keywords as a Variale names.
+# Variable names should be short but descriptive.
+# Be careful when using lowercase letter l and uppercase letter O. because they could be confused with the numbers 1, 0.
+
+
+# Traceback
