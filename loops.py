a991d31fdb40a656c2977f693784843848027a59
lenovo legion 5 pro <lenovolegion6pro@gmail.com>
2023-08-24 17:45:43 +0330

    Learning basic concepts of loops

---
+++ loops.py
@@ -0,0 +1,9 @@
+# Loops in python
+
+# Using for loop to print out each element in a list
+
+languages = ['English', 'French', 'German', 'Japanese', 'Spanish', 'Italian', 'Chinese']
+for language in languages:      #this line tells python to pull a name from the list languages, and associate it with the variable language
+    # For every language in languages...
+    print(language)
+# Any lines of code after the for loop that are not indented are executed once without repetition.
